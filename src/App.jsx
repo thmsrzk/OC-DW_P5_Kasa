9c1bcbeeee229dc5227638f482e28f2978c9022b
thmsrzk <thoms.rzk@gmail.com>
2024-04-22 10:25:23 +0200

    Finished Header (except for link href, empty for now)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './styles/App.scss'
 import router from './components/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
+import Header from './components/Header.jsx'
 
 
 function App() {
-
-  return <RouterProvider router={router}/>
+  return (
+    <>
+    <Header />
+    <RouterProvider router={router}/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../assets/images/Kasa_Logo.png';
+import "../styles/Header.scss";
+
+function Header() {
+  return (
+    <>
+    <header>
+        <nav>
+            <img src={logo} alt="logo" id='banner_logo'/>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">A propos</a></li>
+            </ul>
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default Header
