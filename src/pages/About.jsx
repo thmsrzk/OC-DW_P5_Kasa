a1cac1fe0fc503f8a72fe079859db99fc2e6ad7e
thmsrzk <thoms.rzk@gmail.com>
2024-04-24 15:01:05 +0200

    Home page : Housings Gallery is now displayed with Grid. About : Added Service description.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,8 @@ function About() {
       Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
       <TitleDescription title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. 
       Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-      <TitleDescription title="Service" description="???" />
+      <TitleDescription title="Service" description="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+      Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
       <TitleDescription title="Sécurité" description="La sécurité est la priorité de Kasa. 
       Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. 
       En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
