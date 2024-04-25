36fc331385cf3a18c3abfae9945edc449b699d13
thmsrzk <thoms.rzk@gmail.com>
2024-04-25 10:19:53 +0200

    About collapse's components have now a max-width to fit the model better.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,16 +8,18 @@ function About() {
   return (
     <section id='about'>
       <Banner imgSrc={aboutBanner} imgAlt=""/>
-      <Collapse title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. 
-      Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
-      <Collapse title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. 
-      Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-      <Collapse title="Service" description="La bienveillance fait partie des valeurs fondatrices de Kasa. 
-      Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-      <Collapse title="Sécurité" description="La sécurité est la priorité de Kasa. 
-      Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. 
-      En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
-      Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+      <div className='about-collapse-container'>
+        <Collapse title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. 
+        Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+        <Collapse title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+        Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+        <Collapse title="Service" description="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+        Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+        <Collapse title="Sécurité" description="La sécurité est la priorité de Kasa. 
+        Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. 
+        En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
+        Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+      </div>
  
     </section>
   )
