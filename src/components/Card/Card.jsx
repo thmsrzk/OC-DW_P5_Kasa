cf507cc7d8186785347037d16d57e437146bfa4d
thmsrzk <thoms.rzk@gmail.com>
2024-04-30 18:11:54 +0200

    Home cards : Cards now contain a NavLink redirecting to expected path ('location path' + card ID) Removed figcaption background and added a black background to the figure and an opacity of 0.8 to the img so we can see the text better.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import "./Card.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Card.scss";
 
-
-function Cards({card}) {
+function Cards({ card, linkBase }) {
   return (
     <>
+      <Link to={`${linkBase}${card.id}`}>
         <figure key={card.id} className="Cards">
           <img src={card.cover} alt={card.title} />
           <figcaption>
             <p>{card.title}</p>
           </figcaption>
         </figure>
+      </Link>
     </>
   );
-};
+}
 
-export default Cards
+export default Cards;
