7c691e1c6fdb9653ab8ea73ddfb667abfbb091ba
thmsrzk <thoms.rzk@gmail.com>
2024-05-13 13:58:47 +0200

    Slider component : function "toggleImage" is now separated into two functions "prevImage" & "nextImage"

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
 import "./Slider.scss";
-import leftChevron from "../../assets/icons/left-arrow.svg";
-import rightChevron from "../../assets/icons/right-arrow.svg";
+import leftArrow from "../../assets/icons/left-arrow.svg";
+import rightArrow from "../../assets/icons/right-arrow.svg";
 
 function Slider({ pictures }) {
   const [sliderIndex, setSliderIndex] = useState(1);
 
-  function toggleImage(indexPayload) {
+  function prevImage() {
     let newState;
-    if (indexPayload + sliderIndex > pictures.length) {
-      newState = 1;
-    } else if (indexPayload + sliderIndex < 1) {
+    if (sliderIndex - 1 < 1) {
       newState = pictures.length;
     } else {
-      newState = sliderIndex + indexPayload;
+      newState = sliderIndex - 1;
+    }
+    setSliderIndex(newState);
+  }
+
+  function nextImage() {
+    let newState;
+    if (sliderIndex + 1 > pictures.length) {
+      newState = 1;
+    } else {
+      newState = sliderIndex + 1;
     }
     setSliderIndex(newState);
   }
@@ -21,11 +29,11 @@ function Slider({ pictures }) {
   return (
     <div className="slider">
         <img src={pictures[sliderIndex - 1]} alt={`Slide ${sliderIndex}`} className="slider-img"/>
-        <button onClick={() => toggleImage(-1)} className="navigation-button prev-button">
-            <img src={leftChevron} alt="previous image" />
+        <button onClick={prevImage} className="navigation-button prev-button">
+            <img src={leftArrow} alt="previous image" />
         </button>
-        <button onClick={() => toggleImage(+1)} className="navigation-button next-button">
-            <img src={rightChevron} alt="next image" />
+        <button onClick={nextImage} className="navigation-button next-button">
+            <img src={rightArrow} alt="next image" />
         </button>
         <p className="index-info">{sliderIndex} / {pictures.length}</p>
     </div>
